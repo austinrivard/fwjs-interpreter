c4d8d1056a3df4ea3be479e1a926047f12cbe8d7
austin <56987226+austinrivard@users.noreply.github.com>
2022-10-20 12:56:38 -0700

    install testing frameworks, start on some tests

---
+++ part1/testSrc/edu/sjsu/fwjs/ValueTest.java
@@ -0,0 +1,16 @@
+package edu.sjsu.fwjs;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ValueTest {
+    @Test
+    public void testClosureVal() {
+        // ClosureVal closureVal = new ClosureVal(
+        //     List.of(3),
+        //     new PrintExpr,
+        //     null
+        // );
+    }
+}
